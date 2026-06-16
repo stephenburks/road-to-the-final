b22a5c0df41cb8c7fd0cd640d04834713ae4522b
Stephen Burks <burksy.dev@gmail.com>
2026-06-16 08:49:21 -0700

    chore: add React Doctor scope config and Prettier ESLint integration (Phase A)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,12 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+  eslintConfigPrettier,
   {
     files: ['**/*.{js,jsx}'],
     extends: [
